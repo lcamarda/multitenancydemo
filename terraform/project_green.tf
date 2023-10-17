1168d4fe904f351290ab91326a916213e60fd1e0
lcamarda <camarda.luca@gmail.com>
2023-10-17 15:28:21 -0400

    Create project_green.tf

---
+++ terraform/project_green.tf
@@ -0,0 +1,10 @@
+resource "nsxt_policy_project" "project-green" {
+  display_name        = "project-green"
+  nsx_id              = "project-green"
+  description         = "Terraform provisioned Project"
+  short_id            = "green"
+  tier0_gateway_paths = ["/infra/tier-0s/parent-t0"]
+  site_info {
+     edge_cluster_paths  = ["/infra/sites/default/enforcement-points/default/edge-clusters/9d4c32ae-25f4-47ac-a98e-74c4832869b0"]
+  }
+}

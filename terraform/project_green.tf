b2c20a57916aa5b351e9363b5b8734faabe6988c
lcamarda <camarda.luca@gmail.com>
2023-10-18 13:00:28 -0400

    Update project_green.tf

--- terraform/project_green.tf
+++ terraform/project_green.tf
@@ -6,5 +6,7 @@ resource "nsxt_policy_project" "project-green" {
   tier0_gateway_paths = ["/infra/tier-0s/parent-t0"]
   site_info {
      edge_cluster_paths  = ["/infra/sites/default/enforcement-points/default/edge-clusters/9d4c32ae-25f4-47ac-a98e-74c4832869b0"]
-  }
+     }
+  depends_on = [nsxt_policy_tier0_gateway.parent-t0 ]
 }
+
